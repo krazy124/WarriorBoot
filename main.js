c2324b5e91b50a4914b571286f5a8b4ff47189da
krazy124 <william.meredith4@gmail.com>
2024-08-11 09:05:43 -0500

    update

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let large = 0.5;
 let view = document.getElementById("view");
 
-/*
 let viewWidth = view.offsetWidth;
 let viewHeight = view.offsetHeight;
 if(viewWidth<576){view.innerHTML= "xs";}
@@ -10,4 +9,3 @@ if(viewWidth>=768 && viewWidth<992){view.innerHTML= "md";}
 if(viewWidth>=992 && viewWidth<1200){view.innerHTML= "lg";}
 if(viewWidth>=1200 && viewWidth<1400){view.innerHTML= "xl";}
 if(viewWidth>=1400){view.innerHTML= "xxl";}
-*/

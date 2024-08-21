f5a938981ad619765eb4c4ce5116d674f13c0df3
krazy124 <william.meredith4@gmail.com>
2024-08-21 17:02:21 -0500

    change font sizes and response

--- main.js
+++ main.js
@@ -3,9 +3,26 @@ let view = document.getElementById("view");
 
 let viewWidth = view.offsetWidth;
 let viewHeight = view.offsetHeight;
-if(viewWidth<576){view.innerHTML= "xs";}
-if(viewWidth>=576 && viewWidth<768){view.innerHTML= "sm";}
-if(viewWidth>=768 && viewWidth<992){view.innerHTML= "md";}
-if(viewWidth>=992 && viewWidth<1200){view.innerHTML= "lg";}
-if(viewWidth>=1200 && viewWidth<1400){view.innerHTML= "xl";}
-if(viewWidth>=1400){view.innerHTML= "xxl";}
+
+view.innerHTML = view.offsetWidth;
+
+/*
+if (viewWidth < 576) {
+  view.innerHTML = "less than 576";
+}
+if (viewWidth >= 576 && viewWidth < 768) {
+  view.innerHTML = "between 576 and 768";
+}
+if (viewWidth >= 768 && viewWidth < 992) {
+  view.innerHTML = "between 768 and 992";
+}
+if (viewWidth >= 992 && viewWidth < 1200) {
+  view.innerHTML = "between 992 and 1200";
+}
+if (viewWidth >= 1200 && viewWidth < 1400) {
+  view.innerHTML = "between 1200 and 1400";
+}
+if (viewWidth >= 1400) {
+  view.innerHTML = "more than 1400";
+}
+*/
